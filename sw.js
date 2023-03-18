e451b07288b728e18c854d83f8bdb7f94698bdd4
sieri <sieri@windowslive.com>
2023-03-18 14:11:39 +0000

    Deploying to gh-pages from @ sieri/FactoryManagementUtils@379283022c0e15cc6ee82fb1099405758b67ebcd 🚀

---
+++ sw.js
@@ -0,0 +1,25 @@
+const cacheName = 'egui-template-pwa';
+const filesToCache = [
+  './',
+  './index.html',
+  './factory_management_utils.js',
+  './factory_management_utils_bg.wasm',
+];
+
+/* Start the service worker and cache all the app's content */
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function (cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+/* Serve cached content when offline */
+self.addEventListener('fetch', function (e) {
+  e.respondWith(
+    caches.match(e.request).then(function (response) {
+      return response || fetch(e.request);
+    })
+  );
+});
